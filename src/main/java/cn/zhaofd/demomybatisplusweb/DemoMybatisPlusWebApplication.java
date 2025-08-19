cca2700ae872e764e84f353f1127ea7a3b5509bb
Tobe-Wang <hzbeq@qq.com>
2025-08-19 16:47:34 +0800

    feat: 添加基础配置和示例代码
    - 新增 application.yml及环境特定配置文件
    - 添加 CacheConfig、DateFormatter 和 GlobalExceptionHandler 配置类
    - 新增 logback.xml 日志配置
    - 添加 MySQL 数据库和 Redis 缓存配置
    - 新增示例数据库脚本
    - 添加 README.md 项目说明文件
    - 新增 Git 相关配置文件
    - 新增 Maven Wrapper 配置和脚本
    - 新增项目启动类 DemoMybatisPlusWebApplication

---
+++ src/main/java/cn/zhaofd/demomybatisplusweb/DemoMybatisPlusWebApplication.java
@@ -0,0 +1,13 @@
+package cn.zhaofd.demomybatisplusweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMybatisPlusWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMybatisPlusWebApplication.class, args);
+    }
+
+}
