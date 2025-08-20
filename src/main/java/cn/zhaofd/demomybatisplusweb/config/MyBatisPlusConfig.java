8b84f4efed43ff3b3666cc8624b7aacff3a6eae9
Tobe-Wang <hzbeq@qq.com>
2025-08-20 16:32:02 +0800

    refactor: 更新 MyBatis-Plus 配置类注释- 在 MyBatisPlusConfig 类的 @MapperScan 注解上添加注释 - 解释为何使用 @MapperScan 而非在每个 mapper 接口上使用 @Mapper - 提及 Spring Boot官方文档的推荐做法

--- src/main/java/cn/zhaofd/demomybatisplusweb/config/MyBatisPlusConfig.java
+++ src/main/java/cn/zhaofd/demomybatisplusweb/config/MyBatisPlusConfig.java
@@ -11,6 +11,6 @@ import org.springframework.context.annotation.Configuration;
  * MyBatis-Plus配置类
  */
 @Configuration
-@MapperScan("cn.zhaofd.demomybatisplusweb.modules.demo.mapper")
+@MapperScan("cn.zhaofd.demomybatisplusweb.modules.demo.mapper") // Spring Boot官方文档明确推荐使用@MapperScan方式，不推荐接口上注释@Mapper方式(单独初始化，当有大量@Mapper时（50+），启动时间显著增加)
 public class MyBatisPlusConfig {
 }
