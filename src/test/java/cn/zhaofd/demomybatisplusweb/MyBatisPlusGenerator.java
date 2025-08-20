d74c7fcd23c349125972edb830f896697b4913fe
Tobe-Wang <hzbeq@qq.com>
2025-08-20 15:39:58 +0800

    build(demo-mybatis-plus-web): 优化代码生成配置并调整输出路径
    - 调整代码结构，优化配置顺序
    - 设置 Mapper XML 文件生成路径为 src/main/resources
    - 移除不必要的注释代码

--- src/test/java/cn/zhaofd/demomybatisplusweb/MyBatisPlusGenerator.java
+++ src/test/java/cn/zhaofd/demomybatisplusweb/MyBatisPlusGenerator.java
@@ -5,9 +5,11 @@
 package cn.zhaofd.demomybatisplusweb;
 
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * MyBatis-Plus代码生成器
@@ -42,29 +44,33 @@ public class MyBatisPlusGenerator {
      * 输出目录
      */
     private static final String outputDir = "src\\main\\java";
-//    private static final String outputDir = "E:\\011Code\\021core\\demo-mybatis-plus-web\\src\\main\\java";
 
     /**
      * 代码生成
      */
     public static void main(String[] args) {
-        FastAutoGenerator.create(url, username, password)
+        FastAutoGenerator.create(url, username, password) // 数据库基础配置
+                // 数据库可选配置
+                .dataSourceConfig(builder -> {
+                    // 类型转换器
+                    builder.typeConvertHandler((globalConfig, typeRegistry, metaInfo) -> {
+//                        // 自定义类型转换
+//                        int typeCode = metaInfo.getJdbcType().TYPE_CODE;
+//                        if (typeCode == Types.SMALLINT) {
+//                            return DbColumnType.INTEGER;
+//                        }
+                        return typeRegistry.getColumnType(metaInfo);
+                    });
+                })
                 // 全局配置
                 .globalConfig(builder -> {
                     builder.disableOpenDir() // 禁止自动打开输出目录
-                            .author(author) // 设置作者
+                            .outputDir(outputDir) // 指定代码生成的输出目录
+                            .author(author) // 设置作者名
 //                            .enableSwagger() // 开启swagger模式
-                            .outputDir(outputDir) // 指定输出目录
-//                            .commentDate("yyyy-MM-dd") // 注释日期
+//                            .commentDate("yyyy-MM-dd") // 设置注释日期格式，默认值yyyy-MM-dd
                     ;
-                }).dataSourceConfig(builder -> builder.typeConvertHandler((globalConfig, typeRegistry, metaInfo) -> {
-//                    // 自定义类型转换
-//                    int typeCode = metaInfo.getJdbcType().TYPE_CODE;
-//                    if (typeCode == Types.SMALLINT) {
-//                        return DbColumnType.INTEGER;
-//                    }
-                    return typeRegistry.getColumnType(metaInfo);
-                }))
+                })
                 // 包配置
                 .packageConfig(builder -> {
                     builder.parent("cn.zhaofd.demomybatisplusweb.modules") // 设置父包名，默认值: com.baomidou
@@ -75,7 +81,7 @@ public class MyBatisPlusGenerator {
                             .service("service") // 设置Service接口包名，默认值: service
                             .serviceImpl("service.impl") // 设置Service实现类包名，默认值: service.impl
                             .controller("web") // 设置Controller包名，默认值: controller
-//                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D://")) // 设置Mapper XML生成路径
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, "src\\main\\resources")) // 设置Mapper XML生成路径
                     ;
                 })
                 // 策略配置
@@ -91,7 +97,9 @@ public class MyBatisPlusGenerator {
                             .controllerBuilder() // 启用Controller策略配置
                             .enableRestStyle() // 启用REST风格
                     ;
-                }).templateEngine(new FreemarkerTemplateEngine()) // 设置模板引擎，包括：VelocityTemplateEngine(默认)、FreemarkerTemplateEngine、BeetlTemplateEngine、EnjoyTemplateEngine
+                })
+                // 自定义模板支持配置
+                .templateEngine(new FreemarkerTemplateEngine()) // 设置模板引擎，包括：VelocityTemplateEngine(默认)、FreemarkerTemplateEngine、BeetlTemplateEngine、EnjoyTemplateEngine
                 .execute();
     }
 }
