6df38455ba6941d554357489795b5fc6e379333b
Tobe-Wang <hzbeq@qq.com>
2025-08-30 10:00:03 +0800

    feat(demo): 添加 SysParam 实体的保存接口
    - 在 SysParamController 中添加了 save 方法，用于保存前端传入的 JSON 对象数据
    - 使用 @Valid 注解对传入的 DTO 进行参数校验
    - 异常处理：若参数校验不通过，则抛出 HttpException
    - 返回保存后的 DTO 对象，若保存失败则返回 null

--- src/main/java/cn/zhaofd/demomybatisplusweb/modules/demo/web/SysParamController.java
+++ src/main/java/cn/zhaofd/demomybatisplusweb/modules/demo/web/SysParamController.java
@@ -4,10 +4,15 @@
 
 package cn.zhaofd.demomybatisplusweb.modules.demo.web;
 
+import cn.zhaofd.core.net.exception.HttpException;
+import cn.zhaofd.core.spring.validation.ValidationUtil;
 import cn.zhaofd.demomybatisplusweb.modules.demo.entity.SysParam;
 import cn.zhaofd.demomybatisplusweb.modules.demo.service.intf.SysParamService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -51,5 +56,23 @@ public class SysParamController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Collection<SysParam> listByMap(@RequestParam Map<String, Object> columnMap) {
         return sysParamService.listByMap(columnMap);
+    }
+
+    /**
+     * 保存(前端json对象数据)
+     *
+     * @param dto    DTO数据传输对象
+     * @param errors Errors对象
+     * @return 保存后的DTO数据传输对象
+     */
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public SysParam save(@Valid @RequestBody SysParam dto, Errors errors) {
+        // 输入参数验证
+        if (errors.hasErrors()) {
+            throw new HttpException(HttpStatus.BAD_REQUEST.value(), ValidationUtil.getFieldErrorMsg(errors));
+        }
+
+        return sysParamService.save(dto) ? dto : null;
     }
 }
