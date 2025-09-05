a837ecd9ba5f0cdb4bca6d0c308739041b7102db
Tobe-Wang <hzbeq@qq.com>
2025-09-05 15:17:25 +0800

    feat(config): 添加 MyBatis Plus 分页插件
    - 在 MyBatisPlusConfig 类中添加分页插件配置
    - 使用 MybatisPlusInterceptor 和 PaginationInnerInterceptor 实现分页功能
    - 配置 MySQL 数据库类型

--- src/main/java/cn/zhaofd/demomybatisplusweb/config/MyBatisPlusConfig.java
+++ src/main/java/cn/zhaofd/demomybatisplusweb/config/MyBatisPlusConfig.java
@@ -4,7 +4,11 @@
 
 package cn.zhaofd.demomybatisplusweb.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,4 +17,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("cn.zhaofd.demomybatisplusweb.modules.demo.mapper") // Spring Boot官方文档明确推荐使用@MapperScan方式，不推荐接口上注释@Mapper方式(单独初始化，当有大量@Mapper时（50+），启动时间显著增加)
 public class MyBatisPlusConfig {
+    /**
+     * 添加分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 如果有多数据源可以不配具体类型, 否则都建议配上具体的DbType
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
+        return interceptor;
+    }
 }
