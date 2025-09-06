3245c515117a5083d6de9c1e0cc66a5867ac2ec9
Tobe-Wang <hzbeq@qq.com>
2025-09-06 22:45:06 +0800

    refactor(demo): 更新代码注释
    - 在 SysInfoController、SysInfoMapper、SysInfoService 和 SysInfoServiceImpl 文件中添加了新的注释
    - 新注释说明了代码是使用 MyBatis-Plus 代码生成器生成的，继承了自定义父类并包含了自动填充字段示例

--- src/main/java/cn/zhaofd/demomybatisplusweb/modules/demo/service/intf/SysInfoService.java
+++ src/main/java/cn/zhaofd/demomybatisplusweb/modules/demo/service/intf/SysInfoService.java
@@ -11,6 +11,7 @@ import cn.zhaofd.demomybatisplusweb.modules.demo.entity.SysInfo;
  * <p>
  *  服务类
  * </p>
+ * 直接调用“MyBatis-Plus代码生成器”生成继承自定义父类的代码应用示例+自动填充字段示例
  *
  * @author zhaofd
  * @since 2025-09-06
