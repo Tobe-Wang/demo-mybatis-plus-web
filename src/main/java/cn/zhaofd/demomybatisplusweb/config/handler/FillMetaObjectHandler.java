5acbff04f4cb1b8d22cf40eb45dfa391a0bd00e5
Tobe-Wang <hzbeq@qq.com>
2025-09-06 22:24:14 +0800

    feat(config): 添加字段自动填充处理器
    - 新增 FillMetaObjectHandler 类实现 MetaObjectHandler 接口
    - 在插入数据时自动填充创建时间字段
    - 在更新数据时自动填充更新时间字段

---
+++ src/main/java/cn/zhaofd/demomybatisplusweb/config/handler/FillMetaObjectHandler.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.demomybatisplusweb.config.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * 字段自动填充功能处理器
+ */
+@Component
+public class FillMetaObjectHandler implements MetaObjectHandler {
+    /**
+     * 新增数据填充
+     *
+     * @param metaObject 元数据对象
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "rcreatetime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    /**
+     * 更新数据填充
+     *
+     * @param metaObject 元数据对象
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "rupdatetime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
