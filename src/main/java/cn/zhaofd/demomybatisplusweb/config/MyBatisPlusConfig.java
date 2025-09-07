face7118355d96e399ca41ca74895c7ce9d4cd6a
Tobe-Wang <hzbeq@qq.com>
2025-09-07 22:46:19 +0800

    feat(config): 添加防止全表更新与删除插件
    - 在 MyBatisPlusConfig 中添加 BlockAttackInnerInterceptor
    - 该插件用于阻断全表删除/更新操作，防止恶意攻击
    -优化代码注释，提高可读性

--- src/main/java/cn/zhaofd/demomybatisplusweb/config/MyBatisPlusConfig.java
+++ src/main/java/cn/zhaofd/demomybatisplusweb/config/MyBatisPlusConfig.java
@@ -8,6 +8,7 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.parser.JsqlParserGlobal;
 import com.baomidou.mybatisplus.extension.parser.cache.FstSerialCaffeineJsqlParseCache;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
@@ -36,7 +37,8 @@ public class MyBatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor()); // 【乐观锁插件】
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 【分页插件】如果配置多个插件, 切记分页最后添加；如果有多数据源可以不配具体类型, 否则都建议配上具体的DbType
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 【分页插件】多个同类功能插件，分页最后添加；如果有多数据源可以不配具体类型, 否则都建议配上具体的DbType
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor()); // 【防止全表更新与删除插件】阻断全表删除/更新，阻断永真条件攻击
         return interceptor;
     }
 }
