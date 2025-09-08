901a925c94fc56dc29e262adae2683fc68c36847
Tobe-Wang <hzbeq@qq.com>
2025-09-08 13:50:57 +0800

    refactor(demo): 增加 OtherInfo 类的序列化实现
    - 让 OtherInfo 类实现 Serializable 接口
    - 添加 serialVersionUID 字段以确保序列化的一致性和兼容性

--- src/main/java/cn/zhaofd/demomybatisplusweb/modules/demo/entity/OtherInfo.java
+++ src/main/java/cn/zhaofd/demomybatisplusweb/modules/demo/entity/OtherInfo.java
@@ -6,10 +6,16 @@ package cn.zhaofd.demomybatisplusweb.modules.demo.entity;
 
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 /**
  * json字段类型处理示例-其它信息
  */
 @Data
-public class OtherInfo {
+public class OtherInfo implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     private String phone;
 }
